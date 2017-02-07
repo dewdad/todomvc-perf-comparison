6ea6d4d94c4bd913ac8b10c3d6534ff0fd2ff598
leonwgc <guochao.wang@dianrong.com>
2017-02-07 15:48:18 +0800

    test most popular libs

--- todomvc-benchmark/resources/tests.js
+++ todomvc-benchmark/resources/tests.js
@@ -141,39 +141,39 @@ Suites.push({
 //     ]
 // });
 
-// Suites.push({
-//     name: 'Knockout',
-//     url: 'todomvc/knockoutjs/index.html',
-//     version: '3.1.0',
-//     prepare: function (runner, contentWindow, contentDocument) {
-//         return runner.waitForElement('#new-todo').then(function (element) {
-//             element.focus();
-//             return element;
-//         });
-//     },
-//     tests: [
-//         new BenchmarkTestStep('Adding' + numberOfItemsToAdd + 'Items', function (newTodo, contentWindow, contentDocument) {
-//             var viewModel = contentWindow.viewModel;
-//             for (var i = 0; i < numberOfItemsToAdd; i++) {
-//                 var keyupEvent = document.createEvent('Event');
-//                 keyupEvent.initEvent('keyup', true, true);
-//                 keyupEvent.keyCode = 13;
-//                 viewModel.current('Knockout ------ Something to do ' + i);
-//                 newTodo.dispatchEvent(keyupEvent);
-//             }
-//         }),
-//         new BenchmarkTestStep('CompletingAllItems', function (newTodo, contentWindow, contentDocument) {
-//             var checkboxes = contentDocument.querySelectorAll('.toggle');
-//             for (var i = 0; i < checkboxes.length; i++)
-//                 checkboxes[i].click();
-//         }),
-//         new BenchmarkTestStep('DeletingAllItems', function (newTodo, contentWindow, contentDocument) {
-//             var deleteButtons = contentDocument.querySelectorAll('.destroy');
-//             for (var i = deleteButtons.length - 1; i > -1; i--)
-//                 deleteButtons[i].click();
-//         })
-//     ]
-// });
+Suites.push({
+    name: 'Knockout',
+    url: 'todomvc/knockoutjs/index.html',
+    version: '3.1.0',
+    prepare: function (runner, contentWindow, contentDocument) {
+        return runner.waitForElement('#new-todo').then(function (element) {
+            element.focus();
+            return element;
+        });
+    },
+    tests: [
+        new BenchmarkTestStep('Adding' + numberOfItemsToAdd + 'Items', function (newTodo, contentWindow, contentDocument) {
+            var viewModel = contentWindow.viewModel;
+            for (var i = 0; i < numberOfItemsToAdd; i++) {
+                var keyupEvent = document.createEvent('Event');
+                keyupEvent.initEvent('keyup', true, true);
+                keyupEvent.keyCode = 13;
+                viewModel.current('Knockout ------ Something to do ' + i);
+                newTodo.dispatchEvent(keyupEvent);
+            }
+        }),
+        new BenchmarkTestStep('CompletingAllItems', function (newTodo, contentWindow, contentDocument) {
+            var checkboxes = contentDocument.querySelectorAll('.toggle');
+            for (var i = 0; i < checkboxes.length; i++)
+                checkboxes[i].click();
+        }),
+        new BenchmarkTestStep('DeletingAllItems', function (newTodo, contentWindow, contentDocument) {
+            var deleteButtons = contentDocument.querySelectorAll('.destroy');
+            for (var i = deleteButtons.length - 1; i > -1; i--)
+                deleteButtons[i].click();
+        })
+    ]
+});
 
 // Suites.push({
 //     name: 'Ember',
@@ -257,39 +257,39 @@ Suites.push({
     ]
 });
 
-// Suites.push({
-//     name: 'React',
-//     url: 'todomvc/react/index.html',
-//     version: '0.10.0',
-//     prepare: function (runner, contentWindow, contentDocument) {
-//         contentWindow.Utils.store = function () {}
-//         return runner.waitForElement('#new-todo').then(function (element) {
-//             element.focus();
-//             return element;
-//         });
-//     },
-//     tests: [
-//         new BenchmarkTestStep('Adding' + numberOfItemsToAdd + 'Items', function (newTodo, contentWindow, contentDocument) {
-//             for (var i = 0; i < numberOfItemsToAdd; i++) {
-//                 var keydownEvent = document.createEvent('Event');
-//                 keydownEvent.initEvent('keydown', true, true);
-//                 keydownEvent.which = 13; // VK_ENTER
-//                 newTodo.value = 'React --------- Something to do ' + i;
-//                 newTodo.dispatchEvent(keydownEvent);
-//             }
-//         }),
-//         new BenchmarkTestStep('CompletingAllItems', function (newTodo, contentWindow, contentDocument) {
-//             var checkboxes = contentDocument.querySelectorAll('.toggle');
-//             for (var i = 0; i < checkboxes.length; i++)
-//                 checkboxes[i].click();
-//         }),
-//         new BenchmarkTestStep('DeletingAllItems', function (newTodo, contentWindow, contentDocument) {
-//             var deleteButtons = contentDocument.querySelectorAll('.destroy');
-//             for (var i = deleteButtons.length - 1; i > -1; i--)
-//                 deleteButtons[i].click();
-//         })
-//     ]
-// });
+Suites.push({
+    name: 'React',
+    url: 'todomvc/react/index.html',
+    version: '0.10.0',
+    prepare: function (runner, contentWindow, contentDocument) {
+        contentWindow.Utils.store = function () {}
+        return runner.waitForElement('#new-todo').then(function (element) {
+            element.focus();
+            return element;
+        });
+    },
+    tests: [
+        new BenchmarkTestStep('Adding' + numberOfItemsToAdd + 'Items', function (newTodo, contentWindow, contentDocument) {
+            for (var i = 0; i < numberOfItemsToAdd; i++) {
+                var keydownEvent = document.createEvent('Event');
+                keydownEvent.initEvent('keydown', true, true);
+                keydownEvent.which = 13; // VK_ENTER
+                newTodo.value = 'React --------- Something to do ' + i;
+                newTodo.dispatchEvent(keydownEvent);
+            }
+        }),
+        new BenchmarkTestStep('CompletingAllItems', function (newTodo, contentWindow, contentDocument) {
+            var checkboxes = contentDocument.querySelectorAll('.toggle');
+            for (var i = 0; i < checkboxes.length; i++)
+                checkboxes[i].click();
+        }),
+        new BenchmarkTestStep('DeletingAllItems', function (newTodo, contentWindow, contentDocument) {
+            var deleteButtons = contentDocument.querySelectorAll('.destroy');
+            for (var i = deleteButtons.length - 1; i > -1; i--)
+                deleteButtons[i].click();
+        })
+    ]
+});
 
 // Suites.push({
 //     name: 'Om',
